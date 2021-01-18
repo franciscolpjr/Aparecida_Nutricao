6cb2f6d73e19ea1bfa87be5d844e1e839ff740c2
Francisco Lazaro <franciscolpjr@gmail.com>
2021-01-18 06:22:06 -0300

    Adicionando IMC de todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,7 @@
 var titulo = document.querySelector("#titulo");
 titulo.textContent="Aparecida Nutricionista";
 
-//Calculando o IMC
+//Calculando o IMC do primeiro paciente
 var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
@@ -11,8 +11,66 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+//validando os valores da tela antes de calcular o IMC
+var testePeso = true;
+if(peso<=0 || peso >= 400){
+	console.log("Verificar Peso");
+	testePeso=false;
+	paciente.querySelector(".info-imc").textContent = "Peso Inválido";
+}
 
-//retorno do IMC calculado na página index
-paciente.querySelector(".info-imc").textContent = imc;
+var testeAltura=true;
+if(altura<=0 || altura>=3.00){
+	console.log("Verificar Altura");
+	testeAltura=false;
+	paciente.querySelector(".info-imc").textContent = "Altura Inválida";
+}
+
+if(testePeso && testeAltura){
+	var imc = peso / (altura * altura);
+
+	//retorno do IMC calculado na página index
+	paciente.querySelector(".info-imc").textContent = imc;
+}
+
+
+
+//Calculando o IMC de todos os pacientes
+//querySelectorAll retorna um array
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i=0;i<pacientes.length;i++){
+	
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	//validando os valores da tela antes de calcular o IMC
+	var testePeso = true;
+	if(peso<=0 || peso >= 400){
+		console.log("Verificar Peso");
+		testePeso=false;
+		paciente.querySelector(".info-imc").textContent = "Peso Inválido";
+	}
+
+	var testeAltura=true;
+	if(altura<=0 || altura>=3.00){
+		console.log("Verificar Altura");
+		testeAltura=false;
+		paciente.querySelector(".info-imc").textContent = "Altura Inválida";
+	}
+
+	if(testePeso && testeAltura){
+		var imc = peso / (altura * altura);
+
+		//retorno do IMC calculado na página index
+		paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+	}
+
+}
 
